8f1f76d86c13f0eb3cb4f54b2fcf7f77bbddea5b
Noah Schill <noahrschill@gmail.com>
2024-07-08 22:23:21 -0700

    happy path v1

--- tests/unit_tests/test__routes__error_cases.py
+++ tests/unit_tests/test__routes__error_cases.py
@@ -30,24 +30,24 @@ def test_delete_nonexistent_file(client: TestClient):
 
 def test_get_files_invalid_page_size(client: TestClient):
     """Test that the API returns a 422 error when an invalid page_size query parameter is provided."""
-    response = client.get("/files?page_size=-1")
+    response = client.get("/v1/files?page_size=-1")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    response = client.get(f"/files?page_size={DEFAULT_GET_FILES_MAX_PAGE_SIZE + 1}")
+    response = client.get(f"/v1/files?page_size={DEFAULT_GET_FILES_MAX_PAGE_SIZE + 1}")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
 def test_get_files_page_token_is_mutually_exclusive_with_page_size_and_directory(client: TestClient):
     """Test that the API returns a 422 error when the page_token query parameter is used with page_size or directory."""
-    response = client.get("/files?page_token=token&page_size=10")
+    response = client.get("/v1/files?page_token=token&page_size=10")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert "mutually exclusive" in str(response.json())
 
-    response = client.get("/files?page_token=token&directory=dir")
+    response = client.get("/v1/files?page_token=token&directory=dir")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert "mutually exclusive" in str(response.json())
 
-    response = client.get("/files?page_token=token&page_size=10&directory=dir")
+    response = client.get("/v1/files?page_token=token&page_size=10&directory=dir")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert "mutually exclusive" in str(response.json())
 
@@ -58,6 +58,6 @@ def test_unforeseen_500_error(client: TestClient):
     delete_s3_bucket(TEST_BUCKET_NAME)
 
     # make a request to the API to a route that interacts with the S3 bucket
-    response = client.get("/files")
+    response = client.get("/v1/files")
     assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
     assert response.json() == {"detail": "Internal server error"}
